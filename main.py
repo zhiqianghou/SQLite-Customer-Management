c7bcdf7d93e4a0da1ba27b0843abbb0c1dc1e1db
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-09-25 18:10:26 -0500

    Connect to SQL database, load data, set default index visible False

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
-	 QLineEdit, QPushButton, QMainWindow, QTableWidget
+	 QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 import sys
 from datetime import datetime
 from PyQt6.QtGui import QAction
+import sqlite3
+
 
 
 class MainWindow(QMainWindow): # QMainWindow class allow more operation like toolbar, status bar et al.
@@ -23,8 +25,18 @@ class MainWindow(QMainWindow): # QMainWindow class allow more operation like too
 		self.table = QTableWidget()
 		self.table.setColumnCount(4)
 		self.table.setHorizontalHeaderLabels(("ID","Name","Country","Mobile"))
+		self.table.verticalHeader().setVisible(False)
 		self.setCentralWidget(self.table)
 
+	def load_data(self):
+		connection = sqlite3.connect("database.db")
+		result = connection.execute("select * from patients")
+		self.table.setRowCount(0)
+		for row_number, row_data in enumerate(result):
+			self.table.insertRow(row_number)
+			for column_number, data in enumerate(row_data):
+				self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+		connection.close()
 
 
 
@@ -32,5 +44,6 @@ class MainWindow(QMainWindow): # QMainWindow class allow more operation like too
 app = QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.load_data()
 sys.exit(app.exec())
 
