6576ef6dba577b0236b84bf8fa6df304434d559a
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-09-26 18:25:11 -0500

    Add insert method, InsertDialog class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
-	 QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
+	 QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox
 import sys
 from datetime import datetime
 from PyQt6.QtGui import QAction
@@ -15,8 +15,9 @@ class MainWindow(QMainWindow): # QMainWindow class allow more operation like too
 		file_menu_item = self.menuBar().addMenu("&File")
 		help_menu_item = self.menuBar().addMenu("&Help")
 
-		add_student_action = QAction("Add Student", self)
-		file_menu_item.addAction(add_student_action)
+		add_customer_action = QAction("Add Customer", self)
+		add_customer_action.triggered.connect(self.insert)
+		file_menu_item.addAction(add_customer_action)
 
 		about_action = QAction("About", self)
 		help_menu_item.addAction(about_action)
@@ -38,12 +39,51 @@ class MainWindow(QMainWindow): # QMainWindow class allow more operation like too
 				self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
 		connection.close()
 
+	def insert(self):
+		dialog = InsertDialog()
+		dialog.exec()
+
+
+class InsertDialog(QDialog):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Add Customer Information")
+		self.setFixedWidth(300)
+		self.setFixedHeight(300)
+
+		layout = QGridLayout()
+
+		# Add customer name widget
+		label1 = QLabel("Customer Name:")
+		customer_name = QLineEdit()
+		customer_name.setPlaceholderText("Name:")
+
+		layout.addWidget(label1,0,0)
+		layout.addWidget(customer_name,0,1)
+
+		# Add customer country widget
+		label2 = QLabel("Customer Country:")
+		country_name = QComboBox()
+		countries = ["USA", "Canada", "Mexico", "Europe", "Asia", "Others"]
+		country_name.addItems(countries)
+		layout.addWidget(label2, 1, 0)
+		layout.addWidget(country_name, 1, 1)
+
+
+
+
+
+		self.setLayout(layout)
+
+
+
+
 
 
 
 app = QApplication(sys.argv)
-age_calculator = MainWindow()
-age_calculator.show()
-age_calculator.load_data()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
 
