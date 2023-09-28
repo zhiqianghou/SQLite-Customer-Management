23db850ae1dccf182a67f4286e4f01fe4b4f0b8f
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-09-28 18:02:19 -0500

    Add StatusBar

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
 	 QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox, \
-	 QToolBar
+	 QToolBar, QStatusBar
 import sys
 from datetime import datetime
 from PyQt6.QtGui import QAction, QIcon
@@ -45,6 +45,29 @@ class MainWindow(QMainWindow): # QMainWindow class allow more operation like too
 		toolbar.addAction(add_customer_action)
 		toolbar.addAction(search_action)
 
+		# Create status bar and add status bar elements
+		self.statusbar = QStatusBar()
+		self.setStatusBar(self.statusbar)
+
+		# Detect a cell click
+		self.table.clicked.connect(self.cell_clicked)
+
+
+	def cell_clicked(self):
+		edit_button = QPushButton("Edit Record")
+		edit_button.clicked.connect(self.edit)
+
+		delete_button = QPushButton("Delete Record")
+		delete_button.clicked.connect(self.delete)
+
+		children = self.findChildren(QPushButton)
+		if children:
+			for child in children:
+				self.statusbar.removeWidget(child)
+
+
+		self.statusbar.addWidget(edit_button)
+		self.statusbar.addWidget(delete_button)
 
 	def load_data(self):
 		connection = sqlite3.connect("database.db")
@@ -64,6 +87,13 @@ class MainWindow(QMainWindow): # QMainWindow class allow more operation like too
 		dialog = SearchDialog()
 		dialog.exec()
 
+	def edit(self):
+		dialog = EditDialog()
+		dialog.exec()
+
+	def delete(self):
+		dialog = DeleteDialog()
+		dialog.exec()
 
 
 class InsertDialog(QDialog):
@@ -156,6 +186,16 @@ class SearchDialog(QDialog):
 
 		cursor.close()
 		connection.close()
+
+
+class EditDialog(QDialog):
+	pass
+
+
+class DeleteDialog(QDialog):
+	pass
+
+
 
 
 app = QApplication(sys.argv)
